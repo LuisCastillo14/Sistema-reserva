a3a7f9bdcb1226132a5ac62eb722da77df628037
Luis Castillo <luisangelcastilloplasencia@gmail.com>
2023-10-31 16:02:26 -0500

    Initial commit

---
+++ src/test/java/com/reservas/rerservas/RerservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas.rerservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RerservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
